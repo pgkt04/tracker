125bbf993e34732734f4e07c13f94e255836136e
qtKite <jackywen4@gmail.com>
2021-01-20 15:20:14 +1100

    fixed views

--- tracker/track/serializer.py
+++ tracker/track/serializer.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from .models import Record
 
+class AllRecordsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = [Record]
 
 class RecordSerializer(serializers.ModelSerializer):
     class Meta:
--- tracker/track/views.py
+++ tracker/track/views.py
@@ -1,30 +1,31 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status, generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from track.models import Record
 from track.serializer import RecordSerializer
+import time
 
 
-class GetAllRecords(APIView):
+class GetAllRecords(generics.ListAPIView):
     """
     Returns all records
     """
     queryset = Record.objects.all()
-
-    def get(self, request, format=None):
-        records = Record.objects.all()
-        serializer = RecordSerializer(records)
-        return Response(serializer.data)
-
+    serializer_class = RecordSerializer
 
 class AddRecord(APIView):
     """
     Adds a new record
     """
+
     def post(self, request, format=None):
         serializer = RecordSerializer(data=request.data)
         if serializer.is_valid():
+            serializer.validated_data['created'] = time.time()
+            serializer.validated_data['ended'] = 0
+            serializer.validated_data['uid'] = 1
+            serializer.validated_data['is_active'] = True
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
