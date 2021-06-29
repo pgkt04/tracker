7df0bfd06f15cf7c5d466e6bd67865024835ea9c
qtkite <kite@uwu.icu>
2021-06-29 19:16:10 +1000

    routing files

--- frontend/src/routing.js
+++ frontend/src/routing.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
+// https://www.digitalocean.com/community/tutorials/react-react-router-map-to-routes
+
+const panelComp = Rect.lazy(() => import('./Components/Panel'));
 const private_routes = [
 
 ]
 
-const public_routes = [
+const loginComp = React.lazy(() => import('./Components/Auth/Login'));
+const registerComp = Rect.lazy(() => import('./Components/Auth/Register'));
 
+const public_routes = [
+  { path: '/login', exact: true, name: 'login', component: loginComp },
+  { path: '/register', exact: true, name: 'register', component: registerComp },
+]
+
+const all_routes = [
+  ...public_routes
 ]
