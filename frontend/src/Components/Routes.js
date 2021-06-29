c2ba093de104f2bcac7d5963c3e87731ad48f822
qtkite <kite@uwu.icu>
2021-06-29 14:31:17 +1000

    new routing directory

--- frontend/src/Components/Routes.js
+++ frontend/src/Components/Routes.js
@@ -9,70 +9,70 @@ import Navigation from './Navigation';
 
 export class Routes extends Component {
 
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props)
 
-        this.state = {
-            verified: false,
-            username: '',
-        }
-
-        this.updateVerified = this.updateVerified.bind(this)
+    this.state = {
+      verified: false,
+      username: '',
     }
 
-    async updateVerified() {
-        try {
-            let result = await getUserInfo()
-            this.setState({
-                verified: result.detail === 'success',
-                username: result.username,
-            })
-        } catch (e) {
-            this.setState({
-                verified: false
-            })
-        }
+    this.updateVerified = this.updateVerified.bind(this)
+  }
+
+  async updateVerified() {
+    try {
+      let result = await getUserInfo()
+      this.setState({
+        verified: result.detail === 'success',
+        username: result.username,
+      })
+    } catch (e) {
+      this.setState({
+        verified: false
+      })
+    }
+  }
+
+  async componentDidMount() {
+    this.updateVerified()
+  }
+
+  render() {
+
+    if (this.state.verified) {
+      return (
+        <Router>
+          <Navigation doRefresh={this.updateVerified} user={this.state.username} />
+          <Panel onLogout={this.updateVerified} />
+        </Router>
+      )
     }
 
-    async componentDidMount() {
-        this.updateVerified()
-    }
-
-    render() {
-
-        if (this.state.verified) {
-            return (
-                <Router>
-                    <Navigation doRefresh={this.updateVerified} user={this.state.username} />
-                    <Panel onLogout={this.updateVerified} />
-                </Router>
-            )
-        }
-
-        return (
-            <Router>
-                <Navigation doRefresh={this.updateVerified} />
-                <Route exact path="/">
-                    <Form>
-                        <Form.Group>
-                            <Link to="/login"><Button block>Login</Button></Link>
-                        </Form.Group>
-                        <Form.Group>
-                            <Link to="/register"><Button block>Register</Button></Link>
-                        </Form.Group>
-                    </Form>
-                </Route>
-                <Switch>
-                    <Route path="/login">
-                        <Login onLoginSuccess={this.updateVerified} />
-                    </Route>
-                    <Route path="/register">
-                        <Register onRegisterSuccess={this.updateVerified} />
-                    </Route>
-                </Switch>
-            </Router>
-        )
-    }
+    return (
+      <Router>
+        <Navigation doRefresh={this.updateVerified} />
+        <Route exact path="/">
+          <Form>
+            <Form.Group>
+              <Link to="/login"><Button block>Login</Button></Link>
+            </Form.Group>
+            <Form.Group>
+              <Link to="/register"><Button block>Register</Button></Link>
+            </Form.Group>
+          </Form>
+        </Route>
+        <Switch>
+          <Route path="/login">
+            <Login onLoginSuccess={this.updateVerified} />
+          </Route>
+          <Route path="/register">
+            <Register onRegisterSuccess={this.updateVerified} />
+          </Route>
+        </Switch>
+      </Router>
+    )
+  }
 }
 
 export default Routes
---
+++ frontend/src/routing.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const private_routes = [
+
+]
+
+const public_routes = [
+  
+]
