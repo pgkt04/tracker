5fafa69daa5970b3816747dd87f7544e3dd3e7c3
qtkite <kite@uwu.icu>
2021-06-30 22:28:46 +1000

    topic field

--- tracker/track/models.py
+++ tracker/track/models.py
@@ -5,3 +5,4 @@ class Record(models.Model):
     ended = models.IntegerField(default=0)
     uid = models.IntegerField(default=1)
     is_active = models.BooleanField(default=True)
+    topic = models.CharField(max_length=100)
