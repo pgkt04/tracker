59752b1e0a1b744fad40093e93e79fa29f64a29c
zhwu2697 <zhwu2697@uni.sydney.edu.au>
2021-09-25 03:56:46 +1000

    upgrade yarn

--- frontend/src/Components/Features/Tracker.js
+++ frontend/src/Components/Features/Tracker.js
@@ -1,6 +1,5 @@
 import React, { Component, Fragment } from 'react'
 import { Button, Col, Row, Form } from 'react-bootstrap'
-import { ThemeConsumer } from 'react-bootstrap/esm/ThemeProvider';
 import { Redirect } from 'react-router-dom'
 import { auth_api as api } from '../Api'
 
